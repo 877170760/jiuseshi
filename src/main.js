ec9b1a554afca941711bf6920204a4b45d0d069f
clf <877170760@qq.com>
2023-02-28 11:27:00 +0800

    1

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-
+let vConsole = null;
+vConsole = new VConsole()
 new Vue({
   router,
   render: h => h(App)
